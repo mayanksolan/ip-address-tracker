3b1b52f4f08716d953cac80411251251df3cc270
Mayank Solan <mayank@giveindia.org>
2021-08-30 18:58:33 +0530

    design

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="head-area">
+          <div style={{ paddingTop: "2rem" }}>IP Address Tracker</div>
+          <div className="head-input">
+            <input type="text" placeholder="Search for any IP address or domain" className="head-input-text"></input>
+            <div className="head-input-icon">
+              <svg xmlns="http://www.w3.org/2000/svg" width="11" height="14">
+                <path fill="none" stroke="#FFF" stroke-width="3" d="M2 1l6 6-6 6" />
+              </svg>
+            </div>
+            <div className="head-details">
+              <div className="details-item">
+                <div className="item-type">IP ADDRESS</div>
+                <div className="item-value">192.212.174.101</div>
+              </div>
+              <div className="details-item">
+                <div className="item-type">LOCATION</div>
+                <div className="item-value">Meerut, Uttar Pradesh</div>
+              </div>
+              <div className="details-item">
+                <div className="item-type">TIMEZONE</div>
+                <div className="item-value">UTC +05:30</div>
+              </div>
+              <div className="details-item">
+                <div className="item-type">ISP</div>
+                <div className="item-value">Airtel</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="map-area"></div>
       </header>
     </div>
   );
